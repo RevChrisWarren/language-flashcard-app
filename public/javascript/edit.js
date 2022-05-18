d32e396dca5594cde0b7c5b1ad3fe427c5d9652c
Michael Kendrick <michael@locumsmart.net>
2022-05-18 10:19:50 -0500

    making progress

--- public/javascript/edit.js
+++ public/javascript/edit.js
@@ -9,6 +9,9 @@ const modal = document.getElementById("modal");
 const modalBtn = document.querySelector(".modal-btn");
 const newDeckEntryField = document.querySelector("#new-deck-field");
 const dropDownMenu = document.querySelector(".dropdown-menu");
+const span = document.querySelector(".close");
+const newCardBtn = document.querySelector(".new-card-btn");
+const UpdateBtn = document.querySelector(".edit");
 
 const cardListArr = [];
 
@@ -26,6 +29,19 @@ function saveDeckName(event) {
   newDeckListItem.textContent = newDeckEntry;
   dropDownMenu.append(newDeckListItem);
   modal.style.display = "none";
+  newDeckEntryField.value = "";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+function newCard() {
+  console.log("clear");
+  frontInputEl.value = "";
+  backInputEl.value = "";
+  saveBtn.style.display = "block";
+  UpdateBtn.style.display = "none";
 }
 
 function saveFrontBack(event) {
@@ -34,17 +50,39 @@ function saveFrontBack(event) {
   const backCard = backInputEl.value.trim();
   const listCard = document.createElement("div");
   listCard.classList = "list-card";
+  listCard.id = "list-card";
   if (!frontCard || !backCard) {
     return alert("Error: Please enter in both fields");
   }
-  cardListArr.push(frontInputEl.value + "/" + backInputEl.value);
-  listCard.textContent = frontCard;
+  cardListArr.push(frontInputEl.value);
+  cardListArr.push(backInputEl.value);
+  listCard.textContent = frontCard + " / " + backCard;
   listContainer.append(listCard);
   frontInputEl.value = "";
   backInputEl.value = "";
   console.log(cardListArr);
 }
 
+const listCardContainer = document.querySelector(".list-container");
+listCardContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("list-card")) {
+    const word = e.target.innerHTML.split(" / ");
+    frontInputEl.value = word[0];
+    backInputEl.value = word[1];
+    saveBtn.style.display = "none";
+    UpdateBtn.style.display = "block";
+  }
+});
+
+function updateCard(e) {
+  e.preventDefault();
+  console.log("update");
+  console.log(e.target);
+}
+
+UpdateBtn.addEventListener("click", updateCard);
 saveBtn.addEventListener("click", saveFrontBack);
 addNewDeckBtn.addEventListener("click", addNewDeck);
 modalBtn.addEventListener("click", saveDeckName);
+span.addEventListener("click", closeModal);
+newCardBtn.addEventListener("click", newCard);
