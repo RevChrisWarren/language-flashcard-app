ac6b671f74569955eedb0bfa756a517a28176944
Rchandler1 <98798057+Rchandler1@users.noreply.github.com>
2022-05-19 11:57:07 -0400

    updated seeds with isLearned and carsPerDay

--- seeds/deck-seeds.js
+++ seeds/deck-seeds.js
@@ -5,31 +5,37 @@ const deckData = [
     id: 1,
     name: "Words",
     user_id: "1",
+    cardsPerDay: 10,
   },
   {
     id: 2,
     name: "Verbs 1",
     user_id: "1",
+    cardsPerDay: 5,
   },
   {
     id: 3,
     name: "Verbs 2",
     user_id: "1",
+    cardsPerDay: 3,
   },
   {
     id: 4,
     name: "Adjectives",
     user_id: "2",
+    cardsPerDay: 7,
   },
   {
     id: 5,
     name: "Adjectives 2",
     user_id: "3",
+    cardsPerDay: 10,
   },
   {
     id: 6,
-    name: "Adjectives 3",
+    name: "Special Characters",
     user_id: "4",
+    cardsPerDay: 1,
   },
 ];
 
